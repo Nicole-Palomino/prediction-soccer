ef73cf788624c490cd6c6329b6bc4e748cb444f2
Nicole-Palomino <alessa.kz.fx@gmail.com>
2024-04-29 00:12:14 -0500

    modificación de las predicciones

--- firebase/prediction.py
+++ firebase/prediction.py
@@ -1,4 +1,5 @@
 from scipy.stats import mode
+from collections import Counter
 
 def prediction_victory(model_victoria, X_test_victoria, y_test_victoria, home_team, away_team):
     prediccion_victoria = model_victoria.predict(X_test_victoria)[0]
@@ -22,14 +23,16 @@ def prediction_corner(model_corners, X_test_corners):
     return prediction_corner
 
 def prediction_corners_home(model_corners_equipo_local, X_test_corners_equipo_local, home_away):
-    prediccion_corners_equipo_local = mode(model_corners_equipo_local.predict(X_test_corners_equipo_local))[0][0]
-    prediction_home = str(prediccion_corners_equipo_local)
+    prediccion_corners_equipo_local = model_corners_equipo_local.predict(X_test_corners_equipo_local)
+    promedio = round(sum(prediccion_corners_equipo_local) / len(prediccion_corners_equipo_local), 2)
+    prediction_home = str(promedio)
 
     return prediction_home
 
 def prediction_corners_away(model_corners_equipo_visitante, X_test_corners_equipo_visitante, home_away):
-    prediccion_corners_equipo_visitante = mode(model_corners_equipo_visitante.predict(X_test_corners_equipo_visitante))[0][0]
-    prediction_away = str(prediccion_corners_equipo_visitante)
+    prediccion_corners_equipo_visitante = model_corners_equipo_visitante.predict(X_test_corners_equipo_visitante)
+    promedio = round(sum(prediccion_corners_equipo_visitante) / len(prediccion_corners_equipo_visitante), 2)
+    prediction_away = str(promedio)
 
     return prediction_away
 
@@ -40,9 +43,12 @@ def prediction_yellow_cards(model_tarjetas, X_test_tarjetas):
     return prediction_cards
 
 def prediction_over_under(model_over_under, X_test_over_under):
-    prediccion_over_under = mode(model_over_under.predict(X_test_over_under))[0][0]
+    prediccion_over_under = model_over_under.predict(X_test_over_under)
 
-    if prediccion_over_under == True:
+    frecuencias = Counter(prediccion_over_under)
+    valor_mas_comun = frecuencias.most_common(1)[0][0]
+
+    if valor_mas_comun:
         prediccion_ou = f'over 2.5 goals'
     else:
         prediccion_ou = f'under 2.5 goals'
@@ -50,14 +56,15 @@ def prediction_over_under(model_over_under, X_test_over_under):
     return prediccion_ou
 
 def prediction_goals_home(model_goals_local, X_test_goals_local, home_team):
-    prediccion_goals_local = mode(model_goals_local.predict(X_test_goals_local))[0][0]
+    prediccion_goals_local = model_goals_local.predict(X_test_goals_local)[0][0]
     prediction_home = str(prediccion_goals_local)
 
     return prediction_home
 
 def prediction_goals_away(model_goals_away, X_test_goals_away, away_team):
-    prediccion_goals_away = mode(model_goals_away.predict(X_test_goals_away))[0][0]
-    prediction_away = str(prediccion_goals_away)
+    prediccion_goals_away = model_goals_away.predict(X_test_goals_away)
+    promedio = round(sum(prediccion_goals_away) / len(prediccion_goals_away), 2)
+    prediction_away = str(promedio)
 
     return prediction_away
 
--- firebase/save_data.py
+++ firebase/save_data.py
@@ -1,4 +1,7 @@
-from data_processing import load_data, clean_data, load_data_2
+from data_processing import load_data, clean_data, load_data_2, get_data
+from prediction import prediction_corners_home, prediction_over_under
+from prepare_data import select_home_away, prepare_data_corners_home, prepare_data_over_under
+from models import model_corners_home, model_over_under
 
 # Ruta al archivo CSV
 df1 = "../data/SC0.csv"
@@ -7,11 +10,17 @@ df3 = "../data/SC0(2).csv"
 df4 = "../data/SC0(3).csv"
 df5 = "../data/SC0(4).csv"
 
-nombre_archivo = '../data/liga_escocia_data.csv'
-
+nombre_archivo = '../data/premier_league_data.csv'
+# df = get_data(nombre_archivo)
+# partidos= select_home_away(df, 'West Ham', 'Liverpool')
+# partidos_equipo_local, partidos_equipo_visitante = partidos[0], partidos[1]
+# model = prepare_data_over_under(partidos_equipo_local)
+# X_train_over_under, X_test_over_under, y_train_over_under, y_test_over_under = model[0], model[1], model[2], model[3]
+# corner_home_model = model_over_under(X_train_over_under, y_train_over_under)
+# corner = prediction_over_under(corner_home_model, X_test_over_under)
 # df_load = load_data(df1, df2, df3, df4, df5)
 # clean_data(df_load, nombre_archivo)
 # print('Se guardaron los datos correctamente.')
 # df_load = load_data_2(nombre_archivo)
 # df_unique = df_load['HomeTeam'].unique()
-# print(df_unique)
+# print(corner)
